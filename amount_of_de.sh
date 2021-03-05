fc24cdb947ea3fc23cd7e5db67bb8919636d7e7c
Huub Exel <60732040+hacxpiont@users.noreply.github.com>
2021-03-05 16:28:29 +0100

    Update amount_of_de.sh

--- amount_of_de.sh
+++ amount_of_de.sh
@@ -6,13 +6,12 @@
 # gets the wikipedia page and puts the wikipedia page in a variable
 WIKIPEDIA_PAGE=`wiki-cli -Pl nl Rijksuniversiteit Groningen`
 
-# this line creates an empty file named de_in_rug_nl.txt
+# this line creates an empty (temporary) file named de_in_rug_nl.txt
 touch de_in_rug_nl.txt
 FILEPATH=./de_in_rug_nl.txt
 
-# -f checks if the file exists and is a normal file, since we the file is just created, it exists.
-# the lines basically put only the text in the in the file to make things less complicated.
-# if this is done, you get an error.
+# -f checks if the file exists and is a normal file, since the file is created above here, it exists.
+# the lines basically put only the text in the file to make things less complicated.
 if [ -f "$FILEPATH" ]
 then 
     echo "$WIKIPEDIA_PAGE" > "$FILEPATH"
